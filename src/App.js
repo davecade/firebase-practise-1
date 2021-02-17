71215dc140df335f646f91a31f59a844b0aa813d
Dave <dave.cadelina@outlook.com>
2021-02-17 15:36:58 +1100

    connected App to Firebase so that it keeps track of the user login/logout state. And added a toggle subscription to firebase whenever the App mounts or unmounts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import './App.css';
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import { auth } from './firebase/firebase.utils'
+import { render } from '@testing-library/react';
 
-const HatsPage = () => (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-)
+class App extends Component {
+  
+  constructor() {
+    super()
 
-function App() {
-  return (
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount () {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+      console.log(user)
+    })
+
+    console.log("OPEN", this.unsubscribeFromAuth)
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+    console.log("CLOSE", this.unsubscribeFromAuth)
+  }
+  
+  render() {
+    return (
       <Fragment>
         <Header />
         <Switch>
@@ -22,7 +44,9 @@ function App() {
           <Route path='/signin' component={SignInAndSignUp} />
         </Switch>
       </Fragment>
-  )
+    )
+  }
+
 }
 
 export default App;
